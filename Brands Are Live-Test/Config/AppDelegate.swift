b60af02d8ae99f13d0eab7bab791d6d8588c8163
Dicky Geraldi <>
2024-01-16 18:56:35 +0700

    First Init

---
+++ Brands Are Live-Test/Config/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Brands Are Live-Test
+//
+//  Created by Dicky Geraldi on 16/01/24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
